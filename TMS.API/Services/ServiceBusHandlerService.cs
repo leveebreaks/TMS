b91a38d278be05a60d3ae8d88c903bb6f459043e
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-03-20 21:47:01 +0200

    initial commit; empty api proj and dal proj with taskitem model

---
+++ TMS.API/Services/ServiceBusHandlerService.cs
@@ -0,0 +1,20 @@
+﻿namespace TMS.API.Services;
+
+public interface IMessageBrokerService
+{
+    Task SendMessage(string message);
+    Task ReceiveMessage(string message);
+}
+
+public class ServiceBusHandlerService : IMessageBrokerService
+{
+    public Task SendMessage(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReceiveMessage(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TMS.DAL/Models/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TMS.DAL.Models;
+
+public enum TaskStatus
+{
+    NotStarted,
+    InProgress,
+    Completed
+}
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public TaskStatus Status { get; set; }
+    public string? AssignedTo { get; set; }
+}
