d79a0e12e8663dbbceea1532d437d2271e9b20e9
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-03-24 21:30:32 +0200

    tms infrastructure project, implementing service bus handler

---
+++ TMS.Infrastructure/Messaging/AzureServiceBusHandler.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace TMS.Infrastructure.Messaging;
+
+public class AzureServiceBusHandler(string connectionString) : IMessageBrokerHandler
+{
+    private readonly ServiceBusClient _client = new ServiceBusClient(connectionString);
+
+    public Task SendMessageAsync(string queue, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReceiveMessageAsync(string queue, Func<object, Task> action)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TMS.Infrastructure/Messaging/IMessageBrokerHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace TMS.Infrastructure.Messaging;
+
+public interface IMessageBrokerHandler
+{
+    Task SendMessageAsync(string queue, object message);
+    Task ReceiveMessageAsync(string queue, Func<object, Task> action);
+}
