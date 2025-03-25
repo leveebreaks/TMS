e896ea7d9a664e2beb153cfc3f70f1b6b267c718
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-03-25 12:27:46 +0200

    message proj ref added to infra

--- TMS.Worker/Program.cs
+++ TMS.Worker/Program.cs
@@ -1,9 +1,11 @@
 using TMS.DAL.Repositories;
+using TMS.Domain.Models;
+using TMS.Messages;
 using TMS.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
-builder.Services.AddScoped<ITaskRepository<TaskItem>, TaskRepository>();
+builder.Services.AddScoped<IRepository<TaskItem>, TaskRepository>();
 
 var host = builder.Build();
 
