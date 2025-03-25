7ba4a166c21bf5ac990704d3e99c099b6e968cfc
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-03-25 14:39:21 +0200

    configuring and implementing webapi di and services

--- TMS.API/Program.cs
+++ TMS.API/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using TMS.DAL.Data;
+using TMS.DAL.Repositories;
+using TMS.Domain.Models;
+using TMS.Infrastructure.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var sbConnectionString = builder.Configuration.GetConnectionString("ServiceBusConnectionString");
+var sbTaskCommandQueue = builder.Configuration.GetConnectionString("ServiceBusTaskCommandsQueue");
+var sbTaskEventQueue = builder.Configuration.GetConnectionString("ServiceBusTaskEventsQueue");
+builder.Services.AddSingleton<IMessageBrokerHandler>(provider => 
+    new AzureServiceBusHandler(sbConnectionString, sbTaskCommandQueue, sbTaskEventQueue));
+
+var sqlConnectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
+builder.Services.AddDbContext<TaskDbContext>(options => options.UseSqlServer(sqlConnectionString));
+builder.Services.AddScoped<IRepository<TaskItem>, TaskRepository>();
 
 var app = builder.Build();
 
